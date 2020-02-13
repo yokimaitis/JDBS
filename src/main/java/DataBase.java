cf39222002ace230e663ab3a630dcf80dc4f9b9d
yokimaitis <Moren189la>
2020-02-13 14:46:35 +0300

    12

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -16,7 +16,6 @@ public class DataBase {
                 while (resultSet.next()) {
                     System.out.println(resultSet.getString("autor_name"));
                 }
-
             }
         } catch (SQLException e) {
             System.out.println("Ошибка поиска авторов.....");
@@ -25,7 +24,8 @@ public class DataBase {
     }
 
     public static void printBooks() {
-        String inputAutor = "";;
+        String inputAutor = "";
+        ;
         Integer autorId = 0;
         System.out.println("Введите автора книги:");
         inputAutor = new Scanner(System.in).nextLine();
@@ -37,19 +37,16 @@ public class DataBase {
             if (resultSet != null) {
                 while (resultSet.next()) {
                     autorId = resultSet.getInt("id");
-
                 }
                 if (autorId > 0) {
                     statment = connection.prepareStatement("Select book_name from book WHERE autor_id=" + autorId);
                     resultSet = statment.executeQuery();
                     if (resultSet != null) {
-                        System.out.println("Найденные книги автора "+inputAutor+": ");
+                        System.out.println("Найденные книги автора " + inputAutor + ": ");
                         while (resultSet.next()) {
                             System.out.println(resultSet.getString("book_name"));
-
                         }
                     }
-
                 } else {
                     System.out.println("Такого автора нету в базе....");
                     return;
@@ -59,8 +56,6 @@ public class DataBase {
             System.out.println("Ошибка поиска книг.....");
             e.printStackTrace();
         }
-
-
     }
 
     public static void addAutor() {
@@ -68,32 +63,26 @@ public class DataBase {
         Integer autorId = 0;
         System.out.println("Добавление Автора...");
         System.out.println("");
-
         System.out.println("Введите автора книги:");
         inputAutor = new Scanner(System.in).nextLine();
 
-
-     ////////////////////////
-
-   ////////////////////////////////////////
-
-
-        //////
         try {
-        Connection connection = JDBCConnector.createConnection();
-        PreparedStatement statment = connection.prepareStatement("Select id from autor WHERE autor_name='" + inputAutor + "'");
-        ResultSet resultSet = statment.executeQuery();
-        if (resultSet != null) {
-            while (resultSet.next()) {
-                autorId = resultSet.getInt("id");
+            Connection connection = JDBCConnector.createConnection();
+            PreparedStatement statment = connection.prepareStatement("Select id from autor WHERE autor_name='" + inputAutor + "'");
+            ResultSet resultSet = statment.executeQuery();
+            if (resultSet != null) {
+                while (resultSet.next()) {
+                    autorId = resultSet.getInt("id");
+                }
+                if (autorId == 0) {
+                    connection = JDBCConnector.createConnection();
+                    statment = connection.prepareStatement("INSERT INTO autor (autor_name) VALUES ('" + inputAutor + "');");
+                    if (statment.executeUpdate() > 0) System.out.println("Автор " + inputAutor + " добавлен..");
+                } else {
+                    System.out.println("Такой автор уже есть в базе.");
+                    return;
+                }
             }
-            if (autorId == 0) {
-            connection = JDBCConnector.createConnection();
-            statment = connection.prepareStatement("INSERT INTO autor (autor_name) VALUES ('" + inputAutor + "');");
-            if (statment.executeUpdate() > 0) System.out.println("Автор " + inputAutor + " добавлен..");
-            } else {System.out.println("Такой автор уже есть в базе.");return;}
-        }
-
         } catch (SQLException e) {
             System.out.println("Ошибка добавления автора.....");
             e.printStackTrace();
@@ -109,8 +98,6 @@ public class DataBase {
         System.out.println("");
         System.out.println("Введите автора книги:");
         inputAutor = new Scanner(System.in).nextLine();
-
-
         try {
             Connection connection = JDBCConnector.createConnection();
 
@@ -119,7 +106,6 @@ public class DataBase {
             if (resultSet != null) {
                 while (resultSet.next()) {
                     autorId = resultSet.getInt("id");
-
                 }
                 if (autorId > 0) {
                     System.out.println("Введите название книги");
@@ -136,6 +122,5 @@ public class DataBase {
             System.out.println("Ошибка добавления книги.....");
             e.printStackTrace();
         }
-
     }
 }
